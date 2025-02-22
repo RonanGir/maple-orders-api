81fd2d4f836f9df7325d7f3e6467af0e9d7f9cea
rgirault <ronan@me.fr>
2025-02-22 14:38:07 +0100

    fix(issue #3) : throw MaplOrdersApiException through RestControllerAdvice

--- src/main/java/com/maplr/test/sugarshack/mapleordersapi/exception/MaplOrdersApiException.java
+++ src/main/java/com/maplr/test/sugarshack/mapleordersapi/exception/MaplOrdersApiException.java
@@ -1,7 +1,11 @@
 package com.maplr.test.sugarshack.mapleordersapi.exception;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
 public class MaplOrdersApiException {
     Integer status;
